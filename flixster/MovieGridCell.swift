2f21f5f63dc901e39644fcd430e190d823422f94
Patricia Booth <44758340+alpacawool@users.noreply.github.com>
2021-10-03 13:51:27 -0700

    Added part 2 user stories

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Trisha on 10/3/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
